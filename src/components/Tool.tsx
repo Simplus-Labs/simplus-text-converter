e4e3cae956176fb7d5539421c51f30440182ed48
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-11-02 19:14:09 -0500

    chore: :building_construction: chore handleClick logic
    add handle Click logic of the tool

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -7,30 +7,21 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { ConverterFunctions } from "@/services/fuctionNames"
-
+import { ConverterFunctionName, ConverterFunctions } from "@/services/fuctionNames"
 
 function Tool() {
     const values = Object.keys(ConverterFunctions)
 
-
-    const handleClick = (e: string) => {
-        console.log(e)
-        switch (e) {
-            case "camelCase":
-                console.log(ConverterFunctions.camelCase("Hola mundo"))
-                break;
-
-            default:
-                break;
-        }
-
+    function handleClick(selectedFunction: ConverterFunctionName) {
+        const inputValue = "Hola mundo Jonnier ALejandro Hola";
+        const result = ConverterFunctions[selectedFunction](inputValue);
+        console.log(result);
     }
 
     return (
         <div>
             <div className="flex items-center justify-between mb-10">
-                <Select onValueChange={(e) => handleClick(e)} >
+                <Select onValueChange={(e) => handleClick(e as ConverterFunctionName)} >
                     <SelectTrigger className="w-[180px]">
                         <SelectValue placeholder="Select a format" />
                     </SelectTrigger>
