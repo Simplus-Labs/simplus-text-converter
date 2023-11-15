1b6f8173e4bc2e1cdcd4615c0e59c4f5934b7383
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-11-14 19:46:20 -0500

    feat: :art: improve de code format
    improve de code format

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ function App(): JSX.Element {
       <div className=" flex flex-col gap-10 w-full">
         <div className="flex flex-col gap-4">
           <h1 className="text-5xl font-bold bg-gradient-to-b from-gradient-start via-gradient-middle to-gradient-end bg-clip-text text-transparent">
-            Text converter: Camelcase, Lowercase, Uppercase
+            Text Converter Tool
           </h1>
-          <div className="text-neutral-700 max-w-3xl">Simplus React Boilerplate by Simplus Labs</div>
+          <div className="text-neutral-700 max-w-3xl">
+            Format your Text: Convert it to Camelcase, Lowercase, Uppercase and More
+          </div>
         </div>
-        <div className="min-h-96 h-96">
+        <div className="min-h-96 h-fit">
           <Tool></Tool>
         </div>
       </div>
